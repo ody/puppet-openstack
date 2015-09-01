37eae9820a67ab5e38a874a040df06283e3beba6
Cody Herriges <cody@herriges.org>
2015-09-01 13:44:40 -0700

    Where to find glance

--- site/profile/manifests/nova/compute.pp
+++ site/profile/manifests/nova/compute.pp
@@ -5,11 +5,12 @@ class profile::nova::compute {
   $rabbit_passwd = hiera('profile::rabbitmq::passwd')
 
   class { '::nova':
-    rabbit_userid   => 'nova',
-    rabbit_password => $rabbit_passwd,
-    rabbit_host     => 'megacon1.localdomain',
-    image_service   => 'nova.image.glance.GlanceImageService',
-    verbose         => true,
+    rabbit_userid      => 'nova',
+    rabbit_password    => $rabbit_passwd,
+    rabbit_host        => 'megacon1.localdomain',
+    image_service      => 'nova.image.glance.GlanceImageService',
+    glance_api_servers => 'megacon1.localdomain:9292',
+    verbose            => true,
   }
 
   class { '::nova::compute': vnc_enabled => true, }
